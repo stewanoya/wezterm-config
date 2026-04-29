75effb3d57e23809a084593327d7252a7ffa02d9
Stew Anoya <s.anoya@draftkings.com>
2026-04-29 10:02:50 -0400

    make tab wider

--- startup.lua
+++ startup.lua
@@ -5,7 +5,7 @@ local M = {}
 function M.apply()
 	wezterm.on("gui-startup", function(cmd)
 		local tab, pane, _ = mux.spawn_window(cmd or {})
-		tab:set_title("⚡")
+		tab:set_title("     ⚡     ")
 		pane:split({
 			direction = "Bottom",
 			size = 0.30,
